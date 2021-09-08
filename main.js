feed108bbbe42ac64877eec5cdf6088d904a9e6a
AdityaSinghTheKing <82265266+AdityaSinghTheKing@users.noreply.github.com>
2021-09-08 18:38:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function SwitchtoMom(){
+    window.location="Mom.html"
+}
+function SwitchtoSister(){
+    window.location="Sister.html"
+}
+function SwitchtoMe(){
+    window.location="adityadabest.html"
+}
+function SwitchtoDad(){
+    window.location="Dad.html"
+}
